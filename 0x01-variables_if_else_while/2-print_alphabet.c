e3ee314c88d49d9dc05634be8fc8d5c8a9e7dced
Onolameh Daniel <djake8991@gmail.com>
2022-04-09 08:48:17 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 /**
  *main - Entry point
  *
- *Return: Always 0 (success)
+ *Return: Always 0 (success/correct)
  */
 int main(void)
 
@@ -10,12 +10,11 @@ int main(void)
 char alphabet;
 
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++);
+{
 putchar(alphabet);
-
+}
 putchar('\n');
 
 return (0);
 
 }
-
-
