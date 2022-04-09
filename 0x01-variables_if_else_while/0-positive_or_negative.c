b2184c04e3d304fa3a3e32d934ecb024efc94773
Onolameh Daniel <djake8991@gmail.com>
2022-04-08 17:02:17 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
  * main - Prints all possible combinations of two different digits,
  *        in ascending order, seperated by a comma followed by a space.
  *
- *        Return: Always 0.
+ *        Return: Always 0(success/correct) 
  */
 int main(void)
 {
@@ -20,7 +20,7 @@ putchar((digit2 % 10) + '0');
 if (digit1 == 8 && digit2 == 9)
 continue;
 
-putchar(',');
+putchar(' ');
 putchar(' ');
 }
 }
