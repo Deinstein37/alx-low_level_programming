af9f3725cf8619bb637de92f8ef7e1a5a4884b79
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-05-03 00:17:22 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,47 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <ctype.h>
 
 /**
- * check_digit - checks if a given char is number or not
- * @a: input char
- * Return: int
- **/
+ * _isnumber - checks if string is a number
+ * @s: string
+ *
+ * Return: On success 1.
+ * If not a number, 0 is returned.
+ */
 
-int check_digit(char *a)
+int _isnumber(char *s)
 {
-	int i, num, len;
+	int i, check, d;
 
-	i = 0;
-	num = 0;
-	len = strlen(a);
-	while (i < len)
-	{
-		if (a[i] < '0' || a[i] > '9')
-		{
-			return (-1);
-		}
-		else
-			num = num * 10 + (a[i] - '0');
+	i = 0, d = 0, check = 1;
+	if (*s == '-')
 		i++;
-	}
-	return (num);
-}
-
-int main(int argc, char *argv[])
-{
-	int i, num, res;
-
-	res = 0;
-	for (i = 1; i < argc; i++)
+	for (; *(s + i) != 0; i++)
 	{
-		num = check_digit(argv[i]);
-		if (num == -1)
+		d = isdigit(*(s + i));
+		if (d == 0)
 		{
-			printf("Error\n");
-			return (1);
+			check = 0;
+			break;
 		}
-		res += num;
 	}
-	printf("%d\n", res);
-	return (0);
+	return (check);
+}
+/**
+ * main - Entry point
+ *
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	int i, n, ex;
+
+	ex = 0, n = 0;
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (_isnumber(argv[i]))
+				n += atoi(argv[i]);
+			else
+				ex = 1;
+		}
+	}
+	if (ex == 0)
+		printf("%i\n", n);
+	else
+		printf("%s\n", "Error");
+	return (ex);
+}
