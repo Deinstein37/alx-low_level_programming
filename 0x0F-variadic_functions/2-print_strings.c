f238dc886e1221aa0fa69c3874795d70a325804a
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-05-19 11:15:05 +0100

    Update 2-print_strings.c

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -1,25 +1,33 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdlib.h>
 #include <stdio.h>
+
 /**
-  * print_strings - print a list of strings
-  * @separator: characters used to delimit
-  * @n: nmber of items to print
-  */
+ * print_strings - prints all strings with a separator
+ * @separator: separe strings
+ * @n: int, number of undefined arguments
+ */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
-	char *words;
-	va_list ap;
+va_list list;
+unsigned int i;
+char *c;
+va_start(list, n);
 
-	va_start(ap, n);
-	for (i = 0; i < n; i++)
-	{
-		words = va_arg(ap, char *);
-		(words) ? printf("%s", words) : printf("(nil)");
-		if (separator != NULL && i < n - 1)
-			printf("%s", separator);
-	}
-	printf("\n");
-	va_end(ap);
+for (i = 0; i < n; i++)
+{
+	c = va_arg(list, char*);
+	if (c != NULL)
+		printf("%s", c);
+	else
+		printf("(nil)");
+
+	if (i != n - 1 && separator != NULL)
+		printf("%s", separator);
+}
+va_end(list);
+
+putchar('\n');
 }
