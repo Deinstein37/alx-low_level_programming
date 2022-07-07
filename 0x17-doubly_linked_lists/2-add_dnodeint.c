2650fae20055fdffe48a4f81a8a516be8cc7db35
Onolameh_Daniel <djake8991@gmail.com>
2022-07-07 10:32:03 -0700

    Write a function that adds a new node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,23 +9,21 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *tmp, *new
+	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (!new)
+
+	if (new_node == NULL)
+	{
+
+		free(new_node);
 		return (NULL);
-		new->n = n;
-		new->next = NULL;
-		if (!*head)
-		}
-			new->prev = NULL;
-			*head = new;
-			return (*head);
-		}	
-		tmp = *head;
-		while (tmp->next)
-			tmp = tmp->next;
-		mp->next = new;
-		tmp->prev = *head;
-		return (new_node);
+	}
+
+	new_node->n = n;
+	new_node->next = *head;
+    new_node->prev = NULL;
+	*head = new_node;
+	
+	return (new_node);
 }
