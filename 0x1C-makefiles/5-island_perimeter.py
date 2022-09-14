d3adbc006b6e7b15a9a097cccf455794c9343aeb
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-09-14 17:25:58 +0100

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
+"""
+5-island_perimeter
+"""
 
 
 def island_perimeter(grid):
-    """Python function to return the perimeter of an island"""
+    """Finds perimeter of island provided as grid"""
+    gridWidth = len(grid)
+    gridLength = len(grid[0])
+    island_count = 0
+    perimeter = 0
+    sides = 4
 
-    p = 0
-    for x in range(len(grid)):
-        for y in range(len(grid[x])):
-            if grid[x][y] == 1:
-                if x + 1 == len(grid) or grid[x + 1][y] == 0:
-                    p += 1
-                if y + 1 == len(grid[0]) or grid[x][y + 1] == 0:
-                    p += 1
-                if x - 1 == -1 or grid[x - 1][y] == 0:
-                    p += 1
-                if y - 1 == -1 or grid[x][y - 1] == 0:
-                    p += 1
-    return(p)
+    if gridLength <= 100 and gridWidth <= 100:
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 1:
+                    island_count += 1
+        if island_count == 0:
+            perimeter = 0
+            return perimeter
+        perimeter = ((sides * island_count)/2) + 2
+        return int(perimeter)
